154671eddb840f0959edc63d8d9f3051f0fef0eb
Parth Shrivastava <shrivastavaparth03@gmail.com>
2026-05-27 17:41:31 +0530

    Create auth service

--- backend/src/modules/auth/auth.service.js
+++ backend/src/modules/auth/auth.service.js
@@ -1,1 +1,10 @@
-import jsonwebtoken from "jsonwebtoken";
+import User from "./user.model.js";
+
+export const createUser = async (name, email, password) => {
+  const newUser = new User({
+    name,
+    email,
+    password,
+  });
+  return await newUser.save();
+};
